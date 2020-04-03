cb60a229bc36e9c7d9501c8bbe651680ecb96083
Kenneth Mui <klmui@wisc.edu>
2020-04-02 21:03:28 -0500

    add core functionality

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
+const name = prompt('What is your name?');
+appendMessage("You joined");
+// Send message to server
+socket.emit('new-user', name);
+
 // Whenever we recieve 'chat-message', do this
 socket.on('chat-message', data => {
-  console.log(data);
+  appendMessage(`${data.name}: ${data.message}`);
 });
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`);
+});
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`);
+});
+
 
 messageForm.addEventListener('submit', e => {
   // Stop form from submitting to server to stop from refreshing
   // If it does refresh, it would lose all of the chat messages
   e.preventDefault();
   const message = messageInput.value;
+  // Show message on your screen
+  appendMessage(`You: ${message}`);
   // Send data from client to server
   socket.emit('send-chat-message', message);
   messageInput.value = '';
 });
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const io = require('socket.io')(3000);
 
+const users = {};
+
 // When a user connects
 io.on('connection', socket => {
-  console.log('New user has connected');
   // Fields can be whatever you want
-  socket.emit('chat-message', 'Welcome!');
+  // socket.emit('chat-message', 'Welcome!');
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    // Make sure to handle on client, sent to all other clients
+    socket.broadcast.emit('user-connected', name);
+  });
   // When it recives an entry of 'send-chat-message' do this
   socket.on('send-chat-message', message => {
     console.log(message);
     // Send message to all other clients on this server except for person that sent the message
-    socket.broadcast.emit('chat-message', message);
+    socket.broadcast.emit('chat-message', { message: message, name:
+    users[socket.id] });
+  });
+  socket.on('disconnect', () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
   });
 });
