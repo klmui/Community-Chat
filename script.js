42b5c2b621a124e1a328c51a1069f6fd2a9a608d
Kenneth Mui <klmui@wisc.edu>
2020-04-03 02:06:26 -0500

    another fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io(process.env.PORT);
+const socket = io("https://serene-archimedes-2c389b.netlify.com");
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
